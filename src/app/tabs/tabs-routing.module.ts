b937512dd6f53f3b9527c5671ab2b2f9c7ae42a3
Jay Visave <jayvisave@gmail.com>
2021-11-20 23:38:56 +0530

    routes changed

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,8 +20,8 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
-        path: 'chat',
-        loadChildren: () => import('../chat/chat.module').then(m => m.ChatPageModule)
+        path: 'home-discussion',
+        loadChildren: () => import('../home-discussion/home-discussion.module').then(m => m.HomeDiscussionPageModule)
       },
       {
         path: 'society-admin-tab3',
